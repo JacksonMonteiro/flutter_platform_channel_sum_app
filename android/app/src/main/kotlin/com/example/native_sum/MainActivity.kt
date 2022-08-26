e3b82926d4688e55d1a40446ed5427ba07c67b14
jacksonmonteiro <infor.jackson324@gmail.com>
2022-08-26 15:37:33 -0300

    integration with android

--- android/app/src/main/kotlin/com/example/native_sum/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_sum/MainActivity.kt
@@ -1,6 +1,28 @@
 package com.example.native_sum
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "jacksonmonteiro.space/native"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call,
+                                 result ->
+
+            if (call.method == "calcSum") {
+                val a = call.argument<Int>("a")?.toInt() ?: 0
+                val b = call.argument<Int>("b")?.toInt() ?: 0
+                result.success(a + b)
+            } else {
+                result.notImplemented();
+            }
+        }
+    }
 }
